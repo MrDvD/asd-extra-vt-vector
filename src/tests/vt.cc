5ddb0ed4a3b4aafc06a53ca20ff839c2279d13cd
MrDvD <ma.labin@yandex.ru>
2026-02-27 23:51:09 +0300

    feat: more test groups

--- src/tests/vt.cc
+++ src/tests/vt.cc
@@ -1,17 +1,44 @@
 #include <gtest/gtest.h>
 #include "../vt/vt.h"
 
-vt::vector<int> h = {5, 7, -3, 0, 1};
+vt::vector<int> array = {5, 7, -3, 0, 1};
 
-TEST(VtVector, Reading) {
-  EXPECT_EQ(h[0], 5);
-  EXPECT_EQ(h[1], 7);
-  EXPECT_EQ(h[4], 1);
+TEST(VtIntVector, BasicElementAccess) {
+  EXPECT_EQ(array[0], 5);
+  EXPECT_EQ(array[1], 7);
+  EXPECT_EQ(array[4], 1);
 
-  EXPECT_EQ(h.at(0), 5);
-  EXPECT_EQ(h.at(2), -3);
-  EXPECT_EQ(h.at(4), 1);
+  EXPECT_EQ(array.at(0), 5);
+  EXPECT_EQ(array.at(2), -3);
+  EXPECT_EQ(array.at(4), 1);
 
-  EXPECT_EQ(h.front(), 5);
-  EXPECT_EQ(h.back(), 1);
+  EXPECT_EQ(array.front(), 5);
+  EXPECT_EQ(array.back(), 1);
+}
+
+TEST(VtIntVector, BasicCapacity) {
+  EXPECT_FALSE(array.empty());
+
+  EXPECT_EQ(array.size(), 5);
+}
+
+TEST(VtIntVector, BasicModifiers) {
+  array.pop_back();
+
+  EXPECT_EQ(array.size(), 4);
+
+  int item = -17;
+  array.push_back(item);
+
+  EXPECT_EQ(array.back(), item);
+  EXPECT_EQ(array.size(), 5);
+
+  item = 3;
+  array.push_back(item);
+  array.push_back(item);
+  array.push_back(item);
+  array.push_back(item);
+
+  EXPECT_EQ(array.back(), item);
+  EXPECT_EQ(array.size(), 9);
 }
